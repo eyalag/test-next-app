0851ad61f3d615f9af138744fd1d4546bcd728e8
Eyal Agiv <eyalag@gmail.com>
2026-05-20 14:31:41 +0300

    asd

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -2,4 +2,6 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  person_profiles: "always",
+  capture_pageview: true,
 });
