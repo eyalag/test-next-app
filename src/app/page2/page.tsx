480ecec9508f48c496d70b69b56474a3e6449b88
Eyal Agiv <eyalag@gmail.com>
2026-05-20 11:45:49 +0300

    t

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -6,6 +6,7 @@ export default function Page2() {
       <Navbar />
       <div className="flex flex-1 items-center justify-center">
         <h1 className="text-4xl font-semibold">Page 2</h1>
+        <p>11</p>
       </div>
     </>
   );
