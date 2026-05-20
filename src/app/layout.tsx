bbed9ea6cd6a7c7fc97a64ee42abf297cc0b4c39
Eyal Agiv <eyalag@gmail.com>
2026-05-20 14:42:30 +0300

    ad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { PostHogProvider } from "@/lib/posthog-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,9 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <PostHogProvider>{children}</PostHogProvider>
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
       <GoogleAnalytics gaId="G-ZPSVR0RFS6" />
     </html>
   );
